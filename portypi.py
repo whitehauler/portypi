497647721ada8818569d31f8ce22ee068fdc8c20
Harishbabu Rengaraj <58879939+reharish@users.noreply.github.com>
2022-03-19 12:38:23 +0530

    Added Basic Socket script

--- portypi.py
+++ portypi.py
@@ -1,11 +1,34 @@
+from sys import argv, exit
 import socket
 
 try:
     conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn.settimeout(2.0)
 except socket.error as err:
     print("Error Creating socket")
 
+# Ports to scan
+ports = [ i for i in range(100) ]
 
-port = [ lambda: i for i in range(1001) ]
+# Open Ports
 
-print(i)
+ports_open = []
+
+
+try: 
+    host_ip = argv[1]
+except socket.gaierror:
+    print("Error Resolving Host")
+    sys.exit()
+
+for port in ports:
+    try:
+        print("Trying " + str(port))
+
+        if conn.connect((host_ip, port)):
+            ports_open.append(port)
+            socket.close()
+    except Exception as os_err:
+        print("Try Bind to Gateway IP \n" + str(os_err) )
+
+print(ports_open)
