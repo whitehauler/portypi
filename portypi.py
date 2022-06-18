ac1f17c58d44775f15ee5edb56a50a533f7ae8ba
reharish <58879939+reharish@users.noreply.github.com>
2022-06-18 22:54:56 +0530

    Updated for scanning 1000 ports.

--- portypi.py
+++ portypi.py
@@ -1,31 +1,37 @@
-from sys import argv, exit
+from sys import argv,exit
 import socket
 
+def usage():
+	print("# PORTYPI")
+	print("USAGE : Require exactly 1 argument.")
+	exit(1)
+
+if len(argv) != 2:
+	usage()
+	
 # Ports to scan
-ports = [ i for i in range(100) ]
+ports = [ i for i in range(1000) ]
 
 # Open Ports
 ports_open = []
+host_ip = argv[1]
 
 
-try: 
-    host_ip = argv[1]
-except socket.gaierror:
-    print("Error Resolving Host")
-    sys.exit()
+print("# Open Ports")
 
 for port in ports:
-    try:
-        conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        conn.settimeout(1320)
-        print("Port " + str(port))
-        if conn.connect((host_ip, port)):
-            print(port)
-            ports_open.append(port)
-    except socket.error as err:
-        print("Error Creating socket")
-    except ConnectionRefusedError:
-        continue
-    conn.close()
-
-print(ports_open)
+	try:
+		conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		conn.settimeout(10)
+		conn.connect((host_ip, port))
+		
+	except ConnectionRefusedError:
+		continue
+	except socket.error:
+		continue
+	except socket.gaierror as err:
+		print("Error Resolving Host: {} ".format(err))
+		exit(2)
+		conn.close()
+		# ports_open.append(port)
+	print(port)
